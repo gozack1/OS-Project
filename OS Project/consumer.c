a15569642213399ac3cde4b841f7911cdc9cafc1
gozack1 <gozack1@users.noreply.github.com>
2016-05-02 16:48:54 -0700

    Update consumer.c

--- OS Project/consumer.c
+++ OS Project/consumer.c
@@ -10,16 +10,32 @@ consumer.c
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #include <buffer.h>
-
 
 sem_t full, empty;
 
 pthread_mutex_t mutex;
 
+int delete_item(buffer_item *item);
+
 void *consumer(void *param)
 {
-  
-  
+  buffer_item item;
+
+  while (1)
+  {
+      int sleep (rand () %5 + 1);
+
+      sem_wait(&full);
+      pthread_mutex_lock(&mutex);
+
+      (delete_item(item) < 0);
+
+      pthread_mutex_unlock(&mutex);
+      sem_post (&empty);
+
+  }
+
 }
