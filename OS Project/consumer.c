5bf270ee3fe705c84d28d7cb8bc72c21a61b2f98
gozack1 <go006@cougars.csusm.edu>
2016-05-02 12:17:21 -0700

    Update consumer.c

--- OS Project/consumer.c
+++ OS Project/consumer.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 
+#include <buffer.h>
+
+
+sem_t full, empty;
+
+pthread_mutex_t mutex;
+
+void *consumer(void *param)
+{
+  
+  
+}
