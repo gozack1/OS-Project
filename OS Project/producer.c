4aa093fac40d21941f8d00a58e619c2f689b9d4e
gozack1 <go006@cougars.csusm.edu>
2016-05-02 12:19:00 -0700

    Update producer.c

--- OS Project/producer.c
+++ OS Project/producer.c
@@ -1,1 +1,24 @@
+/*
+OS Project
+Mikal Callahan and Zachary Go
+Producer-Consumer Problem
+Producer.c
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 
+#include <buffer.h>
+
+
+sem_t full, empty;
+
+pthread_mutex_t mutex;
+
+void *producer(void *param)
+{
+  
+  
+}
