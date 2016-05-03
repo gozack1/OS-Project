fbd72421a20f5a84b79dfd0fb316ff4a48bf51ff
gozack1 <gozack1@users.noreply.github.com>
2016-05-02 17:11:30 -0700

    Update main.c

--- OS Project/main.c
+++ OS Project/main.c
@@ -6,6 +6,15 @@ Producer-Consumer Problem
 main.c
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "buffer.h"
+
 int count,delete = 0; 
 int main ()
 {
