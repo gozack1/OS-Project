f28765edd165cd1940ebf90cc1ee114209ce2f1b
gozack1 <gozack1@users.noreply.github.com>
2016-05-02 17:08:37 -0700

    Update main.c

--- OS Project/main.c
+++ OS Project/main.c
@@ -6,9 +6,25 @@ Producer-Consumer Problem
 main.c
 */
 
-
+int count,delete = 0; 
 int main ()
 {
   
   
+}
+
+int delete_item()
+{
+  buffer_item item;
+  
+  while (count == 0)
+  {
+    pr//do nothing
+  }
+  item = buffer [delete];
+  printf("Consumed  %d ", buffe[delete])
+  delete = (delete + 1 ) % BUFFER_SIZE;
+  
+  count --;
+  return 0;
 }
