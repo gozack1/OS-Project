237f11b21bcf5c378cfe392231d05556133defc1
gozack1 <gozack1@users.noreply.github.com>
2016-05-11 13:25:29 -0700

    Update buffer.h

--- buffer.h
+++ buffer.h
@@ -10,13 +10,8 @@ Producer-Consumer Problem
 #include <semaphore.h>
 #include <pthread.h>
 
-<<<<<<< Updated upstream
-sem_t full, empty; 
-pthread_mutex_t mutex;
-=======
 sem_t full, empty; // delcare full, empty semaphores
 pthread_mutex_t mutex; // declare mutex
->>>>>>> Stashed changes
 
 typedef int buffer_item; // define buffer_item
 #define BUFFER_SIZE 5 // define buffer size
